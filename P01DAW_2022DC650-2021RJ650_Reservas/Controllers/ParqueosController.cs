0580602dbcde10570af9b6a3c29a5835bfddcd53
Gabriel <gabriel.diaz@catolica.edu.sv>
2025-02-27 19:25:08 -0600

    Casi completo, falta verificar el metodo "Validar Credenciales"

---
+++ P01DAW_2022DC650-2021RJ650_Reservas/Controllers/ParqueosController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01DAW_2022DC650_2021RJ650_Reservas.Data;
+using P01DAW_2022DC650_2021RJ650_Reservas.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P01DAW_2022DC650_2021RJ650_Reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParqueosController : ControllerBase
+    {
+        private readonly ParqueosDbContext _context;
+
+        public ParqueosController(ParqueosDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Parqueos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Parqueo>>> GetParqueos()
+        {
+            return await _context.Parqueos.ToListAsync();
+        }
+
+        // GET: api/Parqueos
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Parqueo>> GetParqueo(int id)
+        {
+            var parqueo = await _context.Parqueos.FindAsync(id);
+
+            if (parqueo == null)
+            {
+                return NotFound();
+            }
+
+            return parqueo;
+        }
+
+        // POST: api/Parqueos
+        [HttpPost]
+        public async Task<ActionResult<Parqueo>> PostParqueo(Parqueo parqueo)
+        {
+            _context.Parqueos.Add(parqueo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetParqueo", new { id = parqueo.IdEspacio }, parqueo);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutParqueo(int id, Parqueo parqueo)
+        {
+            if (id != parqueo.IdEspacio)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(parqueo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Parqueos.Any(e => e.IdEspacio == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteParqueo(int id)
+        {
+            var parqueo = await _context.Parqueos.FindAsync(id);
+            if (parqueo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Parqueos.Remove(parqueo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ P01DAW_2022DC650-2021RJ650_Reservas/Controllers/SucursalesController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P01DAW_2022DC650_2021RJ650_Reservas.Models; 
+using P01DAW_2022DC650_2021RJ650_Reservas.Data;
+
+namespace P01DAW_2022DC650_2021RJ650_Reservas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SucursalesController : ControllerBase
+    {
+        private readonly ParqueosDbContext _context;
+
+        public SucursalesController(ParqueosDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sucursales
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sucursal>>> GetSucursales()
+        {
+            return await _context.Sucursales.ToListAsync();
+        }
+
+        // GET: api/Sucursales
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sucursal>> GetSucursal(int id)
+        {
+            var sucursal = await _context.Sucursales.FindAsync(id);
+
+            if (sucursal == null)
+            {
+                return NotFound();
+            }
+
+            return sucursal;
+        }
+
+        // POST: api/Sucursales
+        [HttpPost]
+        public async Task<ActionResult<Sucursal>> PostSucursal(Sucursal sucursal)
+        {
+            _context.Sucursales.Add(sucursal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSucursal", new { id = sucursal.IdSucursal }, sucursal);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSucursal(int id, Sucursal sucursal)
+        {
+            if (id != sucursal.IdSucursal)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sucursal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Sucursales.Any(e => e.IdSucursal == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSucursal(int id)
+        {
+            var sucursal = await _context.Sucursales.FindAsync(id);
+            if (sucursal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sucursales.Remove(sucursal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
